edbeb0856f3e64cd5e460104224afd0f5dc439ca
刘明宇 <532033837@qq.com>
2018-05-09 14:59:02 +0800

    完善异常统一处理类

--- chameleon-config-web/src/main/java/cn/code/chameleon/BaseException.java
+++ chameleon-config-web/src/main/java/cn/code/chameleon/BaseException.java
@@ -1,7 +1,11 @@
 package cn.code.chameleon;
 
 import cn.code.chameleon.common.UnifiedResponse;
+import cn.code.chameleon.enums.ResultCodeEnum;
+import cn.code.chameleon.exception.ChameleonException;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,8 +20,15 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class BaseException {
 
-    @ExceptionHandler(value = Exception.class)
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseException.class);
+
+    @ExceptionHandler(value = Throwable.class)
     public UnifiedResponse defaultErrorHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception {
-        return new UnifiedResponse();
+        if (e instanceof ChameleonException) {
+            return new UnifiedResponse(((ChameleonException) e).getCode(), e.getMessage());
+        } else {
+            LOGGER.error("default error handler: " + e.getMessage());
+            return new UnifiedResponse(ResultCodeEnum.FAILED.getCode(), ResultCodeEnum.FAILED.getMsg());
+        }
     }
 }
